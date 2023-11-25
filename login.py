16c8831780e0ed62a5dc9189d6933a9b5cdb735b
Khan9Tran <trankhangtctv@gmail.com>
2023-11-25 09:52:33 +0700

    them login

--- login.py
+++ login.py
@@ -1,15 +1,49 @@
 import streamlit as st
-import subprocess
+import boto3
+import logging
 
-# Streamlit app
-st.title("AWS IAM Login")
+# Thiết lập logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Kiểm tra xem đã có thông tin xác thực chưa
-if not st.session_state.get('aws_authenticated'):
-    st.warning("Please authenticate with AWS before using the app.")
-
-    # Hiển thị form xác thực
+# Hàm lấy thông tin đăng nhập
+def get_credentials():
     aws_access_key_id = st.text_input("AWS Access Key ID")
     aws_secret_access_key = st.text_input("AWS Secret Access Key", type="password")
-    aws_region = st.text_input("AWS Region")
+    return aws_access_key_id, aws_secret_access_key
 
+# Hàm xác thực và trả về session
+def authenticate(aws_access_key_id, aws_secret_access_key):
+    try:
+        session = boto3.Session(
+            aws_access_key_id=aws_access_key_id,
+            aws_secret_access_key=aws_secret_access_key
+        )
+
+        # Kiểm tra đăng nhập bằng cách gọi lệnh cơ bản
+        s3 = session.client('s3')
+        s3.list_buckets()
+        st.success("Login successful")
+        return session
+    except Exception as e:
+        logger.warning("Error during login")
+        logger.warning(f"Details: {str(e)}")
+        st.warning("Error during login. Please check your credentials.")
+        return None
+
+# Hàm hiển thị form đăng nhập
+def show_login_form():
+    with st.form("login_form"):
+        aws_access_key_id, aws_secret_access_key = get_credentials()
+        submitted = st.form_submit_button("Login")
+        if submitted:
+            session = authenticate(aws_access_key_id, aws_secret_access_key)
+            return session
+    return None
+
+if __name__ == "__main__":
+    session = show_login_form()
+
+    if session is not None:
+        # Thực hiện các thao tác cần thiết với session
+        pass
