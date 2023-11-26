96ee549c6e532d1f8e700822ee6a8b0ced096606
Khan9Tran <trankhangtctv@gmail.com>
2023-11-26 20:28:46 +0700

    fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import boto3
 import login
 import share_file as sf
 import file_control as fc
-
+import time
 
 def list_files(session):
     fc.get_list_files(session)
--- test.py
+++ test.py
@@ -1,45 +1,13 @@
-import os
-import base64
-import boto3
 import streamlit as st
-def download_file_from_s3(session, file_download):
-    if session is not None:
-        s3 = session.client('s3')
+import pandas as pd
+data = [
+    {"Name": "John", "Age": 28, st.button("Download"): "Engineer"},
+    {"Name": "Alice", "Age": 24, st.button("Download"): "Designer"},
+    {"Name": "Bob", "Age": 32, st.button("Download"): "Developer"},
+]
 
-        bucket_name = 'storage-and-sharing-file-kbh'
-        iam_client = session.client('iam')
-        response = iam_client.get_user()
+# Convert the list of dictionaries to a Pandas DataFrame
+df = pd.DataFrame(data)
 
-        username = response["User"]["UserName"]
-        file_name, file_extension = os.path.splitext(file_download)
-        object_key = f"{username}/{file_name}{file_extension}"
-
-        try:
-            # Generate a presigned URL for the S3 object
-            presigned_url = s3.generate_presigned_url(
-                'get_object',
-                Params={'Bucket': bucket_name, 'Key': object_key},
-                ExpiresIn=3600  # URL expiration time in seconds
-            )
-
-            return presigned_url
-
-        except Exception as e:
-            print(f"Failed to generate presigned URL: {e}")
-            return None
-
-def create_download_link(download_url, filename):
-    href = f'<a href="{download_url}" download="{filename}">Click here to download {filename}</a>'
-    return href
-
-import login
-
-if __name__ == '__main__':
-    session = login.show_login_form()
-    download_url = download_file_from_s3(session, 'DSCF6782_750x960.webp')
-
-    if download_url:
-        download_link = create_download_link(download_url, 'DSCF6782_750x960.webp')
-        st.markdown(download_link, unsafe_allow_html=True)
-    else:
-        print("Download failed.")
+# Display the table using st.table()
+st.table(df)
