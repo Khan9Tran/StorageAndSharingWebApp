aa1f6c280462b3588c8acec4df271011701554e1
Khan9Tran <trankhangtctv@gmail.com>
2023-11-29 22:35:15 +0700

    fix file require..

--- upload.py
+++ upload.py
@@ -1,6 +1,7 @@
 import os
 import streamlit as st
 import datetime
+
 def upload_to_s3(uploaded_files, session):
     if session is not None:
         st.write("Uploading files to S3...")
@@ -13,7 +14,7 @@ def upload_to_s3(uploaded_files, session):
             username = response["User"]["UserName"]
             # Create a Boto3 S3 client
             s3 = session.client('s3')
-
+            # Upload file
             for uploaded_file in uploaded_files:
                 # Get the file name and extension
                 file_name, file_extension = os.path.splitext(uploaded_file.name)
@@ -23,7 +24,6 @@ def upload_to_s3(uploaded_files, session):
                 # Upload the file to S3
 
                 s3.upload_fileobj(uploaded_file, bucket_name, s3_key)
-
             st.success("Files uploaded to S3 successfully!")
             st.balloons()
         except Exception as e:
