3050296d069867d623b24f43a2574191806e9105
= <=>
2023-11-29 23:22:54 +0700

    comment

--- delete_file.py
+++ delete_file.py
@@ -1,17 +1,22 @@
 def delete_file(session, filename):
     try:
+        # Tạo đối tượng S3 từ phiên làm việc
         s3 = session.client('s3')
         bucket_name = 'storage-and-sharing-file-kbh'
-        # Delete the file
+
+        # Lấy thông tin đăng nhập IAM
         iam_client = session.client('iam')
         response = iam_client.get_user()
-
         username = response["User"]["UserName"]
+
+        # Xác định đường dẫn đối tượng trong S3
         object_key = f"{username}/{filename}"
+
+        # Xóa file từ S3
         s3.delete_object(Bucket=bucket_name, Key=object_key)
 
-        print(f"The file {filename} has been deleted from {bucket_name}.")
+        print(f"File {filename} đã được xóa khỏi {bucket_name}.")
         return True
     except Exception as e:
-        print(f"An error occurred while deleting the file: {e}")
+        print(f"Có lỗi xảy ra khi xóa file: {e}")
         return False
