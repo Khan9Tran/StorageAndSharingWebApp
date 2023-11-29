72e64ee550254d71275fc85db53c08f11461cb91
= <=>
2023-11-29 22:42:58 +0700

    comment

--- download_file.py
+++ download_file.py
@@ -2,45 +2,51 @@ import os
 from tkinter import Tk, filedialog
 
 def select_folder_dialog():
+    # Tạo một cửa sổ dialog để chọn thư mục
     root = Tk()
-    root.attributes('-topmost', True)
-    root.withdraw()
+    root.attributes('-topmost', True)  # Hiển thị cửa sổ trên cùng
+    root.withdraw()  # Ẩn cửa sổ chính
 
+    # Hiển thị dialog và lấy đường dẫn thư mục được chọn
     folder_selected = filedialog.askdirectory()
 
     # Kiểm tra xem người dùng đã chọn một thư mục hay chọn cancel
     if folder_selected:
-        root.destroy()
+        root.destroy()  # Đóng cửa sổ dialog nếu người dùng đã chọn thư mục
         return folder_selected
     else:
-        root.destroy()
-        
+        root.destroy()  # Đóng cửa sổ dialog nếu người dùng đã chọn cancel
         return None
 
 def download_file_from_s3(session, file_download):
+    # Kiểm tra xem phiên làm việc (session) đã được thiết lập chưa
     if session is not None:
+        # Tạo đối tượng S3 từ phiên làm việc
         s3 = session.client('s3')
+
+        # Mở cửa sổ dialog để chọn thư mục lưu trữ file được tải về
         path = select_folder_dialog()
         if path is None:
-            print(f"Please, select folder")
+            print(f"Vui lòng chọn thư mục")
             return False
+
+        # Xác định tên bucket và đường dẫn đối tượng trong S3
         bucket_name = 'storage-and-sharing-file-kbh'
         iam_client = session.client('iam')
         response = iam_client.get_user()
-
         username = response["User"]["UserName"]
         file_name, file_extension = os.path.splitext(file_download)
         object_key = f"{username}/{file_name}{file_extension}"
 
         try:
-            # Get the user's home directory and create the full path for the local download
-            user_home = os.path.expanduser("~")
-            local_filename = os.path.join(user_home, path, file_download)
+            # Lấy đường dẫn đầy đủ cho file được tải về cục bộ
+            local_filename = os.path.join(path, file_download)
 
-            # Download the file from S3 and save it locally
+            # Tải file từ S3 và lưu trữ nó cục bộ
             s3.download_file(bucket_name, object_key, local_filename)
-            print(f"Download successful. File saved to {local_filename}")
+            print(f"Tải về thành công. File được lưu tại {local_filename}")
         except Exception as e:
-            print(f"Download failed: {e}")
+            print(f"Tải về thất bại: {e}")
             return False
+
     return True
